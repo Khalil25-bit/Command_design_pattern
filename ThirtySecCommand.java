beea96451e8fd3081204a9e69d6f45e2d528b982
Anthony Leeb <aleeb@email.sc.edu>
2023-02-25 22:00:51 -0500

    added Javadoc for ThirtySecCommand class

--- ThirtySecCommand.java
+++ ThirtySecCommand.java
@@ -1,13 +1,19 @@
 /**
+ * Counts down 30 seconds
  * @author Anthony Leeb
  */
 public class ThirtySecCommand implements Command{
   private StopWatch stopwatch;
-  
+  /**
+   * creates a ThirtySecCommand taking in a StopWatch
+   * @param stopWatch an instance of the StopWatch class that has the ability to count down
+   */
   public ThirtySecCommand(StopWatch stopWatch){
     this.stopwatch = stopWatch;
   }
-
+  /**
+   * starts the countdown from thirty seconds 
+   */
   public void execute(){
     stopwatch.countDownThirtySec();
   }
