dbc2517da103c72ced849a642ce8bcd16ef7f801
Jakub Sykora <jakubsykora112@gmail.com>
2023-02-26 11:38:18 -0500

    Removed redundant parameter for startCount and finished javadoc

--- StopWatch.java
+++ StopWatch.java
@@ -4,26 +4,42 @@
 
 import java.util.concurrent.TimeUnit;
 
+/**
+ * Stopwatch class that can initiate and display a countdown from 3 select times
+ */
 public class StopWatch {
     
     private long time;
 
+    /**
+     * Starts a countdown of 30 seconds
+     */
     public void countDownThirtySec() {
       time = 29;
-      startCount(time);
+      startCount();
     }
 
+    /**
+     * Starts a countdown of 1 minute
+     */
     public void countDownOneMin() {
       time = 59;
-      startCount(time); 
+      startCount(); 
     }
 
+    /**
+     * Starts a countdown of 5 minutes
+     */
     public void countDownFiveMin() {
       time = 299;
-      startCount(time);
+      startCount();
     }
 
-    private void startCount(long time) {
+    /**
+     * Initiates a countdown by printing out the given time in the format minutes:seconds
+     * for each loop iteration
+     */
+    private void startCount() {
         try {
             while (time > -1) {
                 System.out.print(String.format("%02d:%02d", (time / 60) % 60, (time % 60)));
