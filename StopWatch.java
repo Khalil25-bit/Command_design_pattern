977a2ecb9c04464714b3b3a50f2fdd3afb13b07b
Jakub Sykora <jakubsykora112@gmail.com>
2023-02-25 21:16:18 -0500

    simplify startCount and finish the 3 countdown methods

--- StopWatch.java
+++ StopWatch.java
@@ -9,31 +9,30 @@ public class StopWatch {
     private long time;
 
     public void countDownThirtySec() {
-
+      time = 299;
+      startCount(time);
     }
 
     public void countDownOneMin() {
-
+      time = 299;
+      startCount(time); 
     }
 
     public void countDownFiveMin() {
-
+      time = 299;
+      startCount(time);
     }
 
     private void startCount(long time) {
-        long s = 0;
-        long m = 0;
         try {
             while (time > -1) {
-                m = (time / 60) % 60;
-                s = (time % 60);
-                System.out.print(String.format("%02d:%02d",m,s));
+                System.out.print(String.format("%02d:%02d", (time / 60) % 60, (time % 60)));
                 TimeUnit.MILLISECONDS.sleep(100);
                 System.out.print("\033[H\033[2J");
                 System.out.flush();
                 time--;
             }
-            System.out.println(String.format("%02d:%02d",m,s));
+            System.out.println(String.format("%02d:%02d", (time / 60) % 60, (time % 60)));
           }
           catch(Exception e) {
             System.out.println(e);
